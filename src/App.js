b9d8174ac7c2ad5e19de81cd198c0469e4aa118f
Ajay Jat <jatajay004@gmail.com>
2021-06-07 00:40:16 +0530

    kslfjd

--- src/App.js
+++ src/App.js
@@ -1,65 +1,73 @@
 import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
-import Header from "./components/header";
-import Content from "./components/content";
-import Homepage from "./components/homepage";
-import ReactLoading from "react-loading";
-
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loader, setLoader] = useState(false);
-  const callBack = () => {
-    setLoader(true);
-    fetch("https://reqres.in/api/users?page=1", {
-      method: "get",
-      headers: { Authentication: "Content-Type:application/json" },
-    })
-      .then((response) => response.json())
-      .then((data1) => {
-        setData([...data1.data]);
-        setTimeout(function () {
-          setLoader(false);
-        }, 1000);
-      })
-      .catch((err) => console.log(err));
-  };
   return (
-    <Grid container direction='column' className='App'>
-      <Grid item>
-        <Header callBack={callBack} />
+    <Grid
+      container
+      lg={6}
+      xl={6}
+      xs={12}
+      sm={8}
+      md={6}
+      direction='column'
+      style={{ borderBottom: "0.5px solid gray" }}
+    >
+      <Grid
+        item
+        style={{
+          background: "red",
+          color: "white",
+          height: "80px",
+          textAlign: "center",
+        }}
+      >
+        <span style={{ fontSize: "50px" }}>FIND A TEAM</span>
       </Grid>
-      <Grid item container style={{ marginTop: "50px" }}>
-        <Grid item sm={2} xs={4} />
-        <Grid item xs={12} sm={8}>
-          {data.length === 0 ? (
-            <div
-              style={{
-                textAlign: "center",
-              }}
-            >
-              <Homepage />
-            </div>
-          ) : loader === true ? (
-            <div style={{ textAlign: "center" }}>
-              <ReactLoading
-                type={"cylon"}
-                color={"purple"}
-                style={{
-                  textAlign: "center",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  marginTop: "50px",
-                  height: "64px",
-                  width: "64px",
-                }}
-              />
-            </div>
-          ) : (
-            <Content data={data} />
-          )}
+      <Grid item container className='fields' style={{ marginTop: "20px" }}>
+        <Grid lg={2} sm={2} xs={1} />
+        <Grid lg={3}>
+          <span style={{ fontSize: "20px" }}>In Game Name</span>
         </Grid>
+        <Grid>
+          <input
+            type='text'
+            placeholder='ENTER IGN'
+            className='button-slanted-content'
+            style={{ border: "1px solid" }}
+          />
+        </Grid>
+      </Grid>
+      <Grid item container className='fields'>
+        <Grid lg={2} sm={2} xs={2} />
+        <Grid lg={3}>
+          <span style={{ fontSize: "20px" }}>Game ID</span>
+        </Grid>
+        <Grid>
+          <input
+            type='text'
+            placeholder='ENTER GAME ID'
+            className='button-slanted-content'
+            style={{ border: "1px solid" }}
+          />
+        </Grid>
+      </Grid>
+      <Grid item container className='fields' style={{ paddingBottom: "80px" }}>
+        <Grid lg={5} sm={2} xs={2} />
+        <input
+          className='button-slanted-content'
+          value='PAY & REGISTER'
+          type='button'
+          style={{
+            background: "red",
+            color: "white",
+            border: "none",
+            height: "30px",
+            fontSize: "15px",
+            cursor: "pointer",
+          }}
+        />
       </Grid>
     </Grid>
   );
